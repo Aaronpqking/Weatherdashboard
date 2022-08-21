4aab8150716e3925fedcb17665613a8ac95e4d89
aKingsView <106622740+aKingsView@users.noreply.github.com>
2022-08-21 02:37:16 -0400

    added 5 day weathe loop

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('https://api.openweathermap.org/data/2.5/forecast?lat=39.961178&lon=-82.998795&exclude=alerts,minutely,hourly&units=imperial&appid=d143de80350b7aaab11bcd65acbca5c0')
+fetch('https://api.openweathermap.org/data/2.5/forecast?lat=39.961178&lon=-82.998795&exclude=minutely,hourly&units=imperial&appid=d143de80350b7aaab11bcd65acbca5c0')
     .then(function (response) {
         return response.json()
     })
@@ -7,15 +7,26 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?lat=39.961178&lon=-82.99
         for (var i = 0; i < data.list.length; i += 8) {
             var day = data.list[i];
             var temp = day.main.temp;
-            console.log(temp);
-            console.log(day);
-            document.getElementById("test").innerText = data.list[i].weather[0].description;
-            console.log(data.list[i].weather[0].description);
-        }
+            var city = data.city.name;
+            var date = new Date(data.list[i].dt_txt).toLocaleDateString('en-us', { weekday: "long", year: "numeric", month: "short", day: "numeric" });
+            var windspeed = data.list[i].wind.speed;
+            var icon = data.list[i].weather[0].icon;
+            var humidity = data.list[i].main.humidity;
+            var description = day.weather[0].description;
 
-        document.getElementById("test").innerText = data.list[0].weather[0].description;
+            // document.getElementById("description").innerText = description;
+            console.log(data.list[i].weather[0].description);
+            document.getElementById("date" + i).innerText = date;
+            document.getElementById("city").innerText = city;
+            document.getElementById("temp" + i).innerText = "TEMP:" + " " + temp;
+            document.getElementById("windspeed" + i).innerText = "Windspeed:" + " " + windspeed;
+            document.getElementById("humidity" + i).innerText = "Humidity:" + " " + humidity;
+            document.getElementById("icon" + i).src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            console.log(icon);
+        }
     })
     .catch(console.error);
+
 
 
 
