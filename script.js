0260bdc1bf023f4c2d562eb06757a2a219428210
aKingsView <106622740+aKingsView@users.noreply.github.com>
2022-09-04 21:46:19 -0400

    updated search history button

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
 console.log(JSON.stringify(searchHistory));
 pageLoad(searchHistory[searchHistory.length - 1] || "columbus");
 
-//     for (var i = 0; i < searchHistory.length; i++) {
-//     lsList(searchHistory[i]);
+    for (var i = 0; i < searchHistory.length; i++) {
+    lsList(searchHistory[i]);
 
-// }
+}
 
 
 function search() {
@@ -15,6 +15,7 @@ function search() {
     searchHistory.push(queryinput);
     localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
     pageLoad(queryinput);
+    lsList();
 }
 
 
@@ -87,9 +88,9 @@ function pageLoad(queryinput) {
                 // console.log(data.list[i].weather[0].description);
                 document.getElementById("date" + i).innerText = date;
                 document.getElementById("city").innerText = city;
-                document.getElementById("temp" + i).innerText = "TEMP:" + " " + temp;
-                document.getElementById("windspeed" + i).innerText = "Windspeed:" + " " + windspeed;
-                document.getElementById("humidity" + i).innerText = "Humidity:" + " " + humidity;
+                document.getElementById("temp" + i).innerText = "TEMP: " + temp + String.fromCharCode(176);
+                document.getElementById("windspeed" + i).innerText = "Windspeed: " + windspeed + " mph";
+                document.getElementById("humidity" + i).innerText = "Humidity: " + humidity + "%";
                 document.getElementById("icon" + i).src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
                 console.log(icon);
             }
@@ -97,75 +98,6 @@ function pageLoad(queryinput) {
         })
 
 
-}
-
-function populateWeather(queryinput) {
-
-    // event.preventDefault();
-
-
-
-    // if (!queryinput) {
-    //     queryinput = 'Columbus';
-    // }
-   
-
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + (queryinput) + '&exclude=minutely,hourly&units=imperial&appid=d143de80350b7aaab11bcd65acbca5c0')
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            console.log(data);
-            for (var i = 0; i < data.list.length; i += 8) {
-                var day = data.list[i];
-                var temp = day.main.temp;
-                var city = data.city.name;
-                var date = new Date(data.list[i].dt_txt).toLocaleDateString('en-us', { weekday: "long", year: "numeric", month: "short", day: "numeric" });
-                var windspeed = data.list[i].wind.speed;
-                var icon = data.list[i].weather[0].icon;
-                var humidity = data.list[i].main.humidity;
-                // var description = day.weather[0].description;
-                 
-
-                // document.getElementById("description").innerText = description;
-                // console.log(data.list[i].weather[0].description);
-                document.getElementById("date" + i).innerText = date;
-                document.getElementById("city").innerText = city;
-                document.getElementById("temp" + i).innerText = "TEMP:" + " " + temp;
-                document.getElementById("windspeed" + i).innerText = "Windspeed:" + " " + windspeed;
-                document.getElementById("humidity" + i).innerText = "Humidity:" + " " + humidity;
-                document.getElementById("icon" + i).src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-                console.log(icon);
-            }
-        })
-        .catch(function (error) {
-            
-            console.log(error)
-        })
-
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + (queryinput) + "&exclude=minutely,hourly&units=imperial&appid=d143de80350b7aaab11bcd65acbca5c0")
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-        console.log(data);
-
-         var city = data.name;
-        //  var date = new Date(data.list[i].dt_txt).toLocaleDateString('en-us', { weekday: "long", year: "numeric", month: "short", day: "numeric" });
-         var windspeed = document.createElement("h2");
-            windspeed.textContent = data.wind.speed;
-        
-         var icon = data.weather[0].icon;
-         var humidity = data.main.humidity;
-
-        //  document.getElementById("date").innerText = date;
-         document.getElementById("city").textContent = city;
-         document.getElementById("temp").textContent = "TEMP:" + " " + temp;
-         document.getElementById("windspeed").append(windspeed); 
-         document.getElementById("humidity").textContent = "Humidity:" + " " + humidity;
-         document.getElementById("icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-
-        })
 }
 
 function lsList(city) {
